4841ab1b305f96cf749777b1964590d619915f61
Riiny Giir <rgiir2020@208-Engineering-3.local>
2019-04-24 10:04:48 -0700

    Snakio

---
+++ Snakio/GameManager.swift
@@ -0,0 +1,13 @@
+//
+//  GameManager.swift
+//  Snakio
+//
+//  Created by Riiny Giir on 4/8/19.
+//  Copyright © 2019 Riiny Giir. All rights reserved.
+//
+
+import SpriteKit
+
+class GameManager {
+    
+}
